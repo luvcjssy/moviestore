960def43bfaf1d47d8817e78b79aa094f40f86ee
Hoan Nguyen <nguyenhoan2510@gmail.com>
2016-02-16 17:14:43 +0700

    commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'carts/show'
+
+  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  resources :movies, only: [:show, :index]
+  root 'movies#index'
+  resource :cart, only: [:show] do
+  	put 'add/:movie_id', to: 'carts#add', as: :add_to
+  	put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
+end
